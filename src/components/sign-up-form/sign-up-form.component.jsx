bcc4585737ca5bf349302d69ad407e65d8f9a86c
Irgen Wiig Sørensen <Irgen_w.s@hotmail.com>
2023-03-09 14:25:39 +0100

    Added a sign in component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -94,7 +94,7 @@ const SignUpForm = () => {
           name='confirmPassword'
           value={confirmPassword} 
         />
-        <Button buttonType={'default'} type="submit">Sign Up</Button>
+        <Button buttonType='default' type="submit">Sign Up</Button>
       </form>
     </div>
   );
