dcb8d8895e8b1400a826ffdf01393f48c068f7a4
Irgen Wiig Sørensen <Irgen_w.s@hotmail.com>
2023-04-03 11:51:29 +0200

    Added Redux thunk to categories as well as a loading spinner to categories

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,23 +1,35 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { selectCategoriesIsLoading } from '../../store/categories/category.selector'
 
 import ProductCard from '../product-card/product-card.component';
+import Spinner from '../spinner/spinner.component'
 import './category-preview.styles.scss'
 
 const CategoryPreview = ( {title, products} ) => {
 
+  const isLoading = useSelector(selectCategoriesIsLoading);
+
   return (
     <div className='category-preview-container'>
-      <h2>
-        <Link className='title' to={title}>{title.toUpperCase()}</Link>
-      </h2>
-      <div className='preview'>
-        {products
-          .filter((_, idx) => idx < 4)
-          .map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </div>
-
+      {
+        isLoading ? (
+          <Spinner /> 
+        ) : (
+        <>
+          <h2>
+            <Link className='title' to={title}>{title.toUpperCase()}</Link>
+          </h2>
+          <div className='preview'>
+            {products
+              .filter((_, idx) => idx < 4)
+              .map((product) => (
+                <ProductCard key={product.id} product={product} />
+                ))}
+          </div>
+        </>)
+      }
     </div>
    )
 };
