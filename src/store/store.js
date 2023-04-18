39ad95b107fb684b72df30cd38e50b13bd0152ee
Irgen Wiig Sørensen <Irgen_w.s@hotmail.com>
2023-04-18 15:30:56 +0200

    Added redux toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,34 +1,35 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
+// import { compose, createStore, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga'
 
-import { rootSaga } from './root-saga';
 import { rootReducer } from './root-reducer';
 
-
-const persistConfig = {
-  key: 'root', 
-  storage,
-  whitelist: ['cart'],
-};
-
-const sagaMiddleware = createSagaMiddleware();
-
-const persistedReducer = persistReducer(persistConfig, rootReducer); 
-
-const middleWares = [process.env.NODE_ENV !== 'production' && logger, sagaMiddleware].filter(
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
 );
 
-const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== 'production' &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   blacklist: ['user'],
+// };
 
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-sagaMiddleware.run(rootSaga);
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-export const persistor = persistStore(store);
+export const store = configureStore({ 
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(middleWares),
+})
+
+// export const persistor = persistStore(store);
